4c4d0a7691377fabece72798d155fdf0717751b1
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-07-24 14:50:59 +0900

    first draft

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ def analyze_images_in_folders(folder_paths):
             if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
                 img_path = os.path.join(folder_path, filename)
                 try:
-                    analysis = DeepFace.analyze(
+                    analyses = DeepFace.analyze(
                         enforce_detection=False,
                         img_path=img_path,
                         actions=['age', 'gender', 'race', 'emotion'],
-                    )[0]  # [0] to get the first face if multiple faces are detected
-                    analysis['file_path'] = img_path
-                    results.append(analysis)
+                    )
+                    for i, analysis in enumerate(analyses):
+                        analysis['file_path'] = img_path
+                        analysis['face_number'] = i + 1  # Add face number starting from 1
+                        results.append(analysis)
                 except Exception as e:
                     errors.append(img_path)
                     print(f"Error analyzing {img_path}: {e}")
@@ -33,7 +35,7 @@ def main():
         "image/adults"
     ]
 
-    repeat = 5
+    repeat = 1
 
     for index in range(repeat):
         results, errors = analyze_images_in_folders(folder_paths)
@@ -49,7 +51,7 @@ def main():
             print("\nImages that could not be analyzed:")
             for error in errors:
                 print(error)
-            with open(f'deepface_analysis_errors_{index}.txt', 'w') as f:
+            with open(f'result/deepface_analysis_errors_{index}.txt', 'w') as f:
                 for error in errors:
                     f.write(f"{error}\n")
             print(f"Error list saved to deepface_analysis_errors_{index}.txt")
